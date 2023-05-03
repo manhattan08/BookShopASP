16756fe651af35c795ba25769cd728c065a0416f
manhattan <andrey2004uor@gmail.com>
2023-05-03 14:19:11 +0300

    ASPSHOP

---
+++ Areas/Admin/Controllers/CategoryController.cs
@@ -0,0 +1,92 @@
+using DataAccess.Repository.IRepository;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using pgShop.Models;
+using Utility;
+
+namespace pgShop.Controllers;
+
+[Area("Admin")]
+[Authorize(Roles = SD.Role_Admin)]
+public class CategoryController : Controller
+{
+    private readonly IUnitOfWork _unitOfWork;
+
+    public CategoryController(IUnitOfWork db)
+    {
+        _unitOfWork = db;
+    }
+    public IActionResult Index()
+    {
+        List<Category> objCategoryList = _unitOfWork.Category.GetAll().ToList();
+        return View(objCategoryList);
+    }
+
+    public IActionResult Create()
+    {
+        return View();
+    }
+    [HttpPost]
+    public IActionResult Create(Category obj)
+    {
+        if (ModelState.IsValid)
+        {
+            _unitOfWork.Category.Add(obj);
+            _unitOfWork.Save();
+            TempData["success"] = "Category created successfully";
+            return RedirectToAction("Index");
+        }
+
+        return View();
+    }
+    
+    public IActionResult Edit(int? id)
+    {
+        if (id == null || id == 0)
+            return NotFound();
+        
+        Category? category = _unitOfWork.Category.Get(u=> u.Id == id);
+        
+        if (category == null)
+            return NotFound();
+        
+        return View(category);
+    }
+    [HttpPost]
+    public IActionResult Edit(Category obj)
+    {
+        if (ModelState.IsValid)
+        {
+            _unitOfWork.Category.Update(obj);
+            _unitOfWork.Save();
+            TempData["success"] = "Category updated successfully";
+            return RedirectToAction("Index");
+        }
+
+        return View();
+    }
+    
+    public IActionResult Delete(int? id)
+    {
+        if (id == null || id == 0)
+            return NotFound();
+        
+        Category? category = _unitOfWork.Category.Get(u=> u.Id == id);
+        
+        if (category == null)
+            return NotFound();
+        
+        return View(category);
+    }
+    [HttpPost,ActionName("Delete")]
+    public IActionResult DeletePost(int? id)
+    {
+        Category? category = _unitOfWork.Category.Get(u=> u.Id == id);
+        if (category == null)
+            return NotFound();
+        _unitOfWork.Category.Remove(category);
+        _unitOfWork.Save();
+        TempData["success"] = "Category deleted successfully";
+        return RedirectToAction("Index");
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,76 @@
+using DataAccess.DBInitializer;
+using DataAccess.Repository;
+using DataAccess.Repository.IRepository;
+using Microsoft.EntityFrameworkCore;
+using pgShop.DataAccess.Data;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.UI.Services;
+using Stripe;
+using Utility;
+
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddControllersWithViews();
+
+builder.Services.AddDbContext<ApplicationDbContext>(o =>
+{
+    o.UseNpgsql(builder.Configuration.GetConnectionString("DefaultConnection"));
+});
+AppContext.SetSwitch("Npgsql.EnableLegacyTimestampBehavior", true);
+
+builder.Services.Configure<StripeSettings>(builder.Configuration.GetSection("Stripe"));
+
+builder.Services.AddIdentity<IdentityUser,IdentityRole>().AddEntityFrameworkStores<ApplicationDbContext>().AddDefaultTokenProviders();
+builder.Services.ConfigureApplicationCookie(options => {
+    options.LoginPath = $"/Identity/Account/Login";
+    options.LogoutPath = $"/Identity/Account/Logout";
+    options.AccessDeniedPath = $"/Identity/Account/AccessDenied";
+});
+
+
+builder.Services.AddDistributedMemoryCache();
+builder.Services.AddSession(options => {
+    options.IdleTimeout = TimeSpan.FromMinutes(100);
+    options.Cookie.HttpOnly = true;
+    options.Cookie.IsEssential = true;
+});
+
+builder.Services.AddScoped<IDbInitializer, DbInitializer>();
+builder.Services.AddRazorPages();
+builder.Services.AddScoped<IUnitOfWork, UnitOfWork>();
+builder.Services.AddScoped<IEmailSender, EmailSender>();
+
+var app = builder.Build();
+
+
+if (!app.Environment.IsDevelopment())
+{
+    app.UseExceptionHandler("/Home/Error");
+    app.UseHsts();
+}
+
+app.UseHttpsRedirection();
+app.UseStaticFiles();
+
+StripeConfiguration.ApiKey = builder.Configuration.GetSection("Stripe:SecretKey").Get<string>();
+app.UseRouting();
+
+app.UseAuthentication();
+app.UseAuthorization();
+app.UseSession();
+SeedDatabase();
+app.MapRazorPages();
+
+app.MapControllerRoute(
+    name: "default",
+    pattern: "{area=Customer}/{controller=Home}/{action=Index}/{id?}");
+
+app.Run();
+
+
+void SeedDatabase() {
+    using (var scope = app.Services.CreateScope()) {
+        var dbInitializer = scope.ServiceProvider.GetRequiredService<IDbInitializer>();
+        dbInitializer.Initialize();
+    }
+}
